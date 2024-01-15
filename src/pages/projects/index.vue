75d98df27fc40be6143507de47043b6ace609f57
Ste79ITA <nastri.s@gmail.com>
2024-01-15 18:48:57 +0100

    Milestone 4

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Latest Projects</h1>
+    </div>
+
+    <div class="container">
+      <div class="grid">
+        <div
+          class="card project-card"
+          v-for="project in projects"
+          :key="project.id"
+        >
+          <h3>Title: {{ project.title }}</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  setup() {
+    return {
+      projects: [],
+      BASE_URL: 'http://127.0.0.1:8000/api',
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(`${this.BASE_URL}/projects`).then((res) => {
+        console.log(res);
+        this.projects = res.data.results;
+        // this.projects = res.data.results.data;
+        console.log(this.projects);
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
